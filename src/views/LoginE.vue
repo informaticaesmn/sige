439e6a3a2382ab05bf18dfb882ca63ca1f29b5ef
InformaticaESMN-Genaro <informaticaesmn@gmail.com>
2025-09-11 09:24:42 -0300

    feat: Vinculacion estudiantes con Firebase Auth.2

--- src/views/LoginE.vue
+++ src/views/LoginE.vue
@@ -84,7 +84,7 @@ import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { signInWithEmailAndPassword } from 'firebase/auth'
 import { auth } from '@/config/firebase'
-import { vincularUsuario } from '@/scripts/vincular'
+import { vincularUsuario } from '@/scripts/vincular.js'
 
 const email    = ref('')
 const password = ref('')
