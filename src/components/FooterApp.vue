52bc88598843b752372cf642f7d4729aed008eb9
InformaticaESMN-Genaro <informaticaesmn@gmail.com>
2025-09-24 08:43:51 -0300

    feat: nuevo Login y el incremental de versiones4

--- src/components/FooterApp.vue
+++ src/components/FooterApp.vue
@@ -3,16 +3,14 @@
   <footer class="bg-gray-700 text-white text-xs flex-1 grid md:grid-cols-2 text-center">
     <div class="flex flex-col md:flex-row items-center justify-center gap-2 p-4">
       <span>Sistema en desarrollo | <i>Versión {{ version }} ({{ commit }})</i>
-      <CodeBracketSquareIcon class="h-5 w-5 inline-block ml-2 mr-4 text-teal-200"/>
-      </span> 
-      
-      <span> <i> InformaticaESMN</i>  </span>
+      <CodeBracketSquareIcon class="h-5 w-5 inline-block ml-2 mr-2 text-teal-200"/>
+      <i>InformaticaESMN</i>  </span>
       <!-- <CubeTransparentIcon class="h-5 w-5 inline-block ml-2 text-teal-200"/> -->
     </div>
     <div class="flex items-center justify-center">
-      <AcademicCapIcon class="h-5 w-5 inline-block ml-2"/>  
+      <AcademicCapIcon class="h-5 w-5 inline-block ml-2 mr-2"/>  
       <span> Escuela Superior de Música de Neuquén &copy; {{ new Date().getFullYear() }}</span>
-      <img src="/avatar.png" alt="Logo ESMN" class="w-14 mx-auto mb-4">
+      <img src="/avatar.png" alt="Logo ESMN" class="w-10 mx-auto m-1">
     </div>
   </footer>
 </template>
